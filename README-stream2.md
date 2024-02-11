6df4af875a8c57917bad451830f3be86b71374fb
Sergey Koksharov <gentoosiastic@gmail.com>
2024-02-11 22:27:28 +0300

    feat: add materials and code samples for 2nd stream

---
+++ README-stream2.md
@@ -0,0 +1,59 @@
+# TypeScript intermediate
+
+## typeof operator
+
+<https://www.typescriptlang.org/docs/handbook/2/typeof-types.html>
+
+## keyof operator
+
+<https://www.typescriptlang.org/docs/handbook/2/keyof-types.html>
+
+## enums vs as const
+
+- [Matt Pocock: Enums considered harmful (2022)](https://www.youtube.com/watch?v=jjMbPt_H3RQ)
+- [Matt Pocock: as const: the most underrated TypeScript feature (2023)](https://www.youtube.com/watch?v=6M9aZzm-kEc)
+
+## Types vs Interfaces
+
+Types by default, interfaces only for object inheritance
+
+- [Differences Between Type Aliases and Interfaces](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#differences-between-type-aliases-and-interfaces)
+- [Matt Pocock: TypeScript: Should you use Types or Interfaces? (2023)](https://www.totaltypescript.com/type-vs-interface-which-should-you-use)
+
+## tuples
+
+<https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types>
+
+## Type-only imports and export
+
+- <https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html>
+- [Consistent Type Imports and Exports: Why and How](https://typescript-eslint.io/blog/consistent-type-imports-and-exports-why-and-how/)
+
+## Intersection types
+
+<https://www.typescriptlang.org/docs/handbook/2/objects.html#intersection-types>
+
+## Type Predicates
+
+<https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates>
+
+## Index signatures
+
+- [Index Signatures](https://www.typescriptlang.org/docs/handbook/2/objects.html#index-signatures)
+- [Dmitri Pavlutin: Index Signatures in TypeScript (2023)](https://dmitripavlutin.com/typescript-index-signatures/)
+- [Expression of type string can't be used to index type X](https://www.totaltypescript.com/concepts/type-string-cannot-be-used-to-index-type)
+- [TypeScript’s Unsung Hero: Index Signatures (2023)](https://blog.herodevs.com/typescripts-unsung-hero-index-signatures-ddc3d1e34c9f)
+
+## Utility Types (Record, Pick, Omit, Partial, Required, Readonly)
+
+- <https://www.typescriptlang.org/docs/handbook/utility-types.html>
+
+## Discriminated Unions
+
+- [What are TypeScript Discriminated Unions? (2023)](https://dev.to/darkmavis1980/what-are-typescript-discriminated-unions-5hbb)
+- [Demystifying TypeScript Discriminated Unions (2022)](https://css-tricks.com/typescript-discriminated-unions/)
+
+## satisfies operator
+
+- <https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html#the-satisfies-operator>
+- [Matt Pocock: Most TS devs don't understand 'satisfies' (2024)](https://www.youtube.com/watch?v=r1L35zxZQPE)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,168 @@
-console.log('hello world');
+// * interface which describes function
+//
+// interface MyFunc {
+//     (value: string): number;
+// }
+
+// * interface declaration merging
+//
+// interface Person {
+//   name: string;
+// }
+
+// interface Person {
+//   age: number;
+// }
+
+// const person: Person = {
+//   name: 'John Doe',
+//   age: 23
+// }
+
+// * as const instead of enums
+//
+// const HTTP_STATUS = {
+//   OK: 200,
+//   BAD_REQUEST: 400,
+//   FORBIDDEN: 403,
+//   NOT_FOUND: 404,
+// } as const;
+
+// type ObjectValues<T> = T[keyof T];
+
+// type HttpStatus = ObjectValues<typeof HTTP_STATUS>;
+
+// function sendResponse(data: unknown, statusCode: HttpStatus) {
+//   if (statusCode === 200) {
+//     // ...
+//   }
+// }
+
+// const roles = ['user', 'moderator', 'admin'] as const;
+
+// * getting type from array values
+
+// type Role = typeof roles[number];
+
+// * typeof
+
+// * type predicates
+
+// const THEME = {
+//   Light: 'light',
+//   Dark: 'dark',
+// } as const;
+
+// type ObjectValues<T> = T[keyof T];
+
+// type Theme = ObjectValues<typeof THEME>;
+
+// type Settings = {
+//   theme: Theme;
+// };
+
+// const initialSettings: Settings = {
+//   theme: THEME.Light,
+// };
+
+// const isTheme = (value: unknown): value is Theme => {
+//   const allowedValues: string[] = Object.values(THEME);
+
+//   return typeof value === 'string' && allowedValues.includes(value);
+// };
+
+// const isSettings = (value: unknown): value is Settings => {
+//   if (value && typeof value === 'object' && 'theme' in value && isTheme(value.theme)) {
+//     return true;
+//   }
+
+//   return false;
+// };
+
+// const loadSettingsFromLs = (lsKey: string, fallback: Settings): Settings => {
+//   try {
+//     const jsonValue = localStorage.getItem(lsKey);
+
+//     if (!jsonValue) {
+//       return fallback;
+//     }
+
+//     const unknownValue: unknown = JSON.parse(jsonValue);
+
+//     if (isSettings(unknownValue)) {
+//       return unknownValue;
+//     }
+
+//     return fallback;
+//   } catch {
+//     return fallback;
+//   }
+// };
+
+// * Indexing
+
+// type Person = { [key: string]: string | number }; // strings, numbers, symbols as key
+// // type Person = Record<string, string | number>;
+
+// const person: Person = {
+//   name: 'John',
+//   age: 23,
+//   occupation: 'entrepreneur',
+// };
+
+// const getFieldFromPerson = (personObj: Person, field: string) => {
+//   return personObj[field];
+// };
+
+// type Person = { [key: string]: string | number }; // strings, numbers, symbols as key
+// type Person = Record<string, string | number>;
+
+// const person = {
+//   name: 'John',
+//   age: 23,
+//   occupation: 'entrepreneur',
+// };
+
+// type Person = typeof person & { [key: string]: string | number };
+
+// const getFieldFromPerson = (personObj: Person, field: string) => {
+//   return personObj[field];
+// };
+
+// * Discriminated unions
+
+// type ApiError = {
+//   status: 'error';
+//   errorMessage: string;
+// };
+
+// type ApiSuccess = {
+//   status: 'success';
+//   data: unknown;
+// };
+
+// type ApiResponse = ApiError | ApiSuccess;
+
+// const processApiAnswer = (response: ApiResponse) => {
+//   if (response.status === 'success') {
+//     console.log(response.data);
+//   } else {
+//     console.log(response.errorMessage);
+//   }
+// };
+
+// * satisfies operator
+
+// const config1: Record<string, string | number | boolean> = {
+//   theme: 'dark',
+//   initialNonogramId: 0,
+//   isMuted: false,
+// };
+
+// config1.theme = 'light';
+
+// const config2 = {
+//   theme: 'dark',
+//   initialNonogramId: 0,
+//   isMuted: false,
+// } satisfies Record<string, string | number | boolean>;
